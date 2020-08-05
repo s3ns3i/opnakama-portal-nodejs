9f2080d5761a6b89e07c73ddf74d82a9d7a27ef5
s3ns3i <eragonsm@gmail.com>
2020-08-05 20:58:01 +0200

    Completed the chat

--- routes/messages.js
+++ routes/messages.js
@@ -4,14 +4,24 @@ const { sequelize } = require('../models');
 const router = express.Router();
 const { models } = sequelize;
 
+const MESSAGE_LIMIT = 200;
+
 /* GET messages listing. */
 router.get('/', (req, res, next) => {
-  models.message.findAll({
-    attributes: ['id', 'sender', 'content', 'createdAt'],
-    raw: true,
-  })
-    .then((messages) => {
-      res.send(messages);
+  models.message.count()
+    .then((messagesNo) => {
+      const options = {
+        attributes: ['id', 'sender', 'content', 'createdAt'],
+        raw: true,
+      };
+      if (MESSAGE_LIMIT < messagesNo) {
+        options.limit = MESSAGE_LIMIT;
+        options.offset = messagesNo - MESSAGE_LIMIT;
+      }
+      models.message.findAll(options)
+        .then((messages) => {
+          res.send(messages);
+        });
     });
 });
 
